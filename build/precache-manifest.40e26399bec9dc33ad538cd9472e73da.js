605dc8d44fa47cb6c449335485d00bd2ab69030e
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-04-09 17:06:20 -0600

    fixed the package.json

---
+++ build/precache-manifest.40e26399bec9dc33ad538cd9472e73da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2446d97936854a5024f0e952555d3c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb9a426a532b8efa3f64",
+    "url": "/static/css/main.bdbbaf90.chunk.css"
+  },
+  {
+    "revision": "dd45d3ee5d9c24566c0c",
+    "url": "/static/js/2.e18b89ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e18b89ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9a426a532b8efa3f64",
+    "url": "/static/js/main.edc9a32a.chunk.js"
+  },
+  {
+    "revision": "d5e8ca3ccac1fc6f8adc",
+    "url": "/static/js/runtime-main.51ac6e37.js"
+  }
+]);
